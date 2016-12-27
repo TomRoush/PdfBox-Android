4903127fc735c0fc552231378aa52b9d56b0953b
Tom Roush <birdbrain2@comcast.net>
2016-12-27 11:36:38 -0600

    Add fontbox unit tests

---
+++ library/src/test/java/com/tom_roush/fontbox/ttf/TestTTFParser.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tom_roush.fontbox.ttf;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * This will test the TTFParser implementation.
+ *
+ * @author Tim Allison
+ */
+public class TestTTFParser extends TestCase
+{
+
+    /**
+     * Check whether the creation date is UTC
+     *
+     * @throws IOException If something went wrong
+     */
+    public void testUTCDate() throws IOException
+    {
+        final InputStream testFile = getClass().getResourceAsStream("/fontbox/ttf/LiberationSans-Regular.ttf");
+        TimeZone utc = TimeZone.getTimeZone("UTC");
+        //Before PDFBOX-2122, TTFDataStream was using the default TimeZone
+        //Set the default to something not UTC and see if a UTC timeZone is returned
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Los Angeles"));
+        TTFParser parser = new TTFParser();
+        TrueTypeFont ttf = parser.parse(testFile);
+        Calendar created = ttf.getHeader().getCreated();
+        assertEquals(created.getTimeZone(), utc);
+
+        Calendar target = Calendar.getInstance(utc);
+        target.set(2012, 9, 4, 11, 2, 31);
+        target.set(Calendar.MILLISECOND, 0);
+        assertEquals(target, created);
+    }
+
+}
